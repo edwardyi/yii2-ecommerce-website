dcf6f99ea52a6daec036e6d374aad130b52a0633
edwardweijieai <edwardweijieai@gmail.com>
2021-08-18 06:23:00 +0000

    [add] gii models

---
+++ common/models/Order.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "orders".
+ *
+ * @property int $id
+ * @property float $total_price
+ * @property int $status
+ * @property string $firstName
+ * @property string $lastName
+ * @property string $email
+ * @property string|null $transaction_id
+ * @property int|null $created_by
+ * @property int|null $updated_by
+ *
+ * @property OrderAddresses[] $orderAddresses
+ * @property OrderItems[] $orderItems
+ * @property User $createdBy
+ */
+class Order extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'orders';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'total_price', 'status', 'firstName', 'lastName', 'email'], 'required'],
+            [['id', 'status', 'created_by', 'updated_by'], 'integer'],
+            [['total_price'], 'number'],
+            [['firstName', 'lastName', 'transaction_id'], 'string', 'max' => 45],
+            [['email'], 'string', 'max' => 255],
+            [['id'], 'unique'],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['created_by' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'total_price' => 'Total Price',
+            'status' => 'Status',
+            'firstName' => 'First Name',
+            'lastName' => 'Last Name',
+            'email' => 'Email',
+            'transaction_id' => 'Transaction ID',
+            'created_by' => 'Created By',
+            'updated_by' => 'Updated By',
+        ];
+    }
+
+    /**
+     * Gets query for [[OrderAddresses]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\OrderAddressesQuery
+     */
+    public function getOrderAddresses()
+    {
+        return $this->hasMany(OrderAddresses::className(), ['order_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[OrderItems]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\OrderItemsQuery
+     */
+    public function getOrderItems()
+    {
+        return $this->hasMany(OrderItems::className(), ['order_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\UserQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'created_by']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \common\models\query\OrderQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \common\models\query\OrderQuery(get_called_class());
+    }
+}
---
+++ common/models/Product.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "products".
+ *
+ * @property int $id
+ * @property string $name
+ * @property float $price
+ * @property string|null $description
+ * @property string|null $image
+ * @property int|null $status
+ * @property int|null $created_at
+ * @property int|null $updated_at
+ * @property int|null $created_by
+ * @property int|null $updated_by
+ *
+ * @property CartItems[] $cartItems
+ * @property OrderItems[] $orderItems
+ * @property User $createdBy
+ * @property User $updatedBy
+ */
+class Product extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'products';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'name', 'price'], 'required'],
+            [['id', 'status', 'created_at', 'updated_at', 'created_by', 'updated_by'], 'integer'],
+            [['price'], 'number'],
+            [['description'], 'string'],
+            [['name'], 'string', 'max' => 255],
+            [['image'], 'string', 'max' => 200],
+            [['id'], 'unique'],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['created_by' => 'id']],
+            [['updated_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['updated_by' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'price' => 'Price',
+            'description' => 'Description',
+            'image' => 'Image',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'created_by' => 'Created By',
+            'updated_by' => 'Updated By',
+        ];
+    }
+
+    /**
+     * Gets query for [[CartItems]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\CartItemsQuery
+     */
+    public function getCartItems()
+    {
+        return $this->hasMany(CartItems::className(), ['product_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[OrderItems]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\OrderItemsQuery
+     */
+    public function getOrderItems()
+    {
+        return $this->hasMany(OrderItems::className(), ['product_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\UserQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'created_by']);
+    }
+
+    /**
+     * Gets query for [[UpdatedBy]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\UserQuery
+     */
+    public function getUpdatedBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'updated_by']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \common\models\query\ProductQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \common\models\query\ProductQuery(get_called_class());
+    }
+}
